2e85dce40ef382b4cd19d12a8249d25e7c674680
Ali McGarry <ali.mcgarry@gmail.com>
2022-03-17 13:56:50 +0000

    Made mobile nav links close mobile nav on click

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict'
 
-const hamburgerBtnOpen = document.querySelector(".hamburger-open");
-const hamburgerBtnClose = document.querySelector(".hamburger-close");
-const mobileNav = document.querySelector(".mobile-nav");
+const hamburgerBtnOpen = document.querySelector(".hamburger-open")
+const hamburgerBtnClose = document.querySelector(".hamburger-close")
+const mobileNav = document.querySelector(".mobile-nav")
+const mobileNavLinks = document.querySelectorAll(".nav-link--mobile")
 
-hamburgerBtnOpen.addEventListener('click', toggleMobileNav);
-hamburgerBtnClose.addEventListener('click', toggleMobileNav);
+hamburgerBtnOpen.addEventListener('click', toggleMobileNav)
+hamburgerBtnClose.addEventListener('click', toggleMobileNav)
+
+mobileNavLinks.forEach((nav) => {
+    nav.addEventListener('click', toggleMobileNav)
+})
 
 function toggleMobileNav () {
     mobileNav.classList.toggle('hidden')
