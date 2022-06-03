100a214f0e8c469d6924a989bb7cde116a12e788
Ali McGarry <ali.mcgarry@gmail.com>
2022-06-03 19:19:17 +0100

    fixed errors

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const hamburger = document.querySelector(".navbar__hamburger");
 const mobileNav = document.querySelector(".navbar__mobileNav");
 const mobileNavLinks = document.querySelectorAll(".navbar__navLink--mobile");
-const contactFormSubmit = document.querySelector("contact__btn");
 
 hamburger.addEventListener("click", toggleMobileNav);
 
@@ -13,19 +12,8 @@ mobileNavLinks.forEach((nav) => {
   nav.addEventListener("click", toggleMobileNav);
 });
 
-contactFormSubmit.addEventListener("submit", (e) => {
-  e.preventDefault();
-  handleContactFormSubmit();
-});
-
 function toggleMobileNav() {
   mobileNav.classList.toggle("navbar__mobileNav--active");
   hamburger.classList.toggle("opened");
   hamburger.setAttribute("aria-expanded", this.classList.contains("opened"));
 }
-
-function handleContactFormSubmit() {
-  readContactFormData();
-}
-
-function readContactFormData() {}
