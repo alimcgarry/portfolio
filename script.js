f9814184fc5ec511fc62d1b5f8aa6af5fe2dc2c8
Ali McGarry <ali.mcgarry@gmail.com>
2022-06-11 15:58:36 +0100

    Made navbar sticky

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function toggleMobileNav() {
   hamburger.classList.toggle("opened");
   hamburger.setAttribute("aria-expanded", this.classList.contains("opened"));
 }
+
+const sectionHeroEl = document.querySelector(".hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-120px",
+  }
+);
+obs.observe(sectionHeroEl);
